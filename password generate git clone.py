19186b69fee0b600b8eb8319e0204fd78ffa252b
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-28 09:02:12 +0545

    Add files via upload

---
+++ password generate git clone.py
@@ -0,0 +1,7 @@
+git clone https://github.com/shubhamggosai/wordlist-creater
+
+git clone https://github.com/Mebus/cupp
+
+git clone https://github.com/prabin4456/cupp999.git
+
+git clone https://github.com/prabin4456/cupp10000.git
