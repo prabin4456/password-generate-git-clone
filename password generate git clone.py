c598bfba10b5e24a79119a5425375c16505d70ca
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-28 09:27:33 +0545

    Update password generate git clone.py

--- password generate git clone.py
+++ password generate git clone.py
@@ -1,7 +1,5 @@
 git clone https://github.com/shubhamggosai/wordlist-creater
-
 git clone https://github.com/Mebus/cupp
-
 git clone https://github.com/prabin4456/cupp999.git
-
 git clone https://github.com/prabin4456/cupp10000.git
+git clone https://github.com/CybernetiX-S3C/Wifi_Passwords-Generator-master
