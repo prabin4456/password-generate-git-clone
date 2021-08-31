590c8d7ed52678ff424d9f4f95b7d87e640f115e
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-31 16:45:16 +0545

    Add files via upload

---
+++ password genarete git clone.py
@@ -0,0 +1,37 @@
+_____           _    __  __
+        |_   _|__   ___ | |   \ \/ /
+          | |/ _ \ / _ \| |____\  /
+          | | (_) | (_) | |____/  \
+          |_|\___/ \___/|_|   /_/\_\ v2.1
+
+
+ =============================================
+|          Install Best Hacking Tool          |
+ =============================================
+
+ [ 1 ] install CeWL
+ [ 2 ] install Hash-Buster
+ [ 3 ] install JohnTheRipper
+ [ 4 ] install JTRE
+ [ 5 ] install SET
+ [ 6 ] install SecLists
+ [ 7 ] install SocialFish
+ [ 8 ] install creddump
+ [ 9 ] install crowbar
+ [ 10 ] install cupp
+ [ 11 ] install findmyhash
+ [ 12 ] install hash-generator
+ [ 13 ] install hashcat
+ [ 14 ] install hasherdotid
+ [ 15 ] install hasher
+ [ 16 ] install hydra
+ [ 17 ] install johnny
+ [ 18 ] install keimpx
+ [ 19 ] install maskprocessor
+ [ 20 ] install patator
+ [ 21 ] install social-engineer-toolkit
+
+ =============================================
+|  00) Back                                   |
+ =============================================
+ ##>
